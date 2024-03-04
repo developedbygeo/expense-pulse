19ad1f839b7ec026ccc959016227d7cdfa5a64bb
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-04 22:35:49 +0100

    Add utils.ts file with cn() and isDevelopment functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const isDevelopment = process.env.NODE_ENV === "development";
