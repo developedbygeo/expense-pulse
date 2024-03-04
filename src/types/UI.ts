5f4d25a6e5d0c7a0e508c9ced12a3f1574e257cc
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-04 22:36:02 +0100

    Add UI types

---
+++ src/types/UI.ts
@@ -0,0 +1,31 @@
+export type WithClassName = {
+  className?: string;
+};
+
+export type WithLocale = {
+  locale: string;
+};
+
+export type WithChildren = {
+  children?: React.ReactNode;
+};
+
+export type WithSource = {
+  src: string;
+};
+
+export type WithDataIsDark = {
+  "data-is-dark"?: string;
+};
+
+export type WithSearchParams = {
+  searchParams?: string | string[] | undefined;
+};
+
+export type WithCategorySearchParams = {
+  searchParams?: {
+    category: string;
+  };
+};
+
+export type AnimationDirection = "up" | "down" | "left" | "right" | "default";
