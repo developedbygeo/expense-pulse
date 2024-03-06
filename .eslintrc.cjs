426bd6053b0de9d97009cbbb4791f8cd02705fc3
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-06 01:53:39 +0100

    Add .txt files to .gitignore and update prettier configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,27 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true, node: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
     ],
-    "no-unused-vars": "warn",
-    "@typescript-eslint/no-unused-vars": "warn"
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'simple-import-sort'],
+    rules: {
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+    },
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+    },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,67 +1,62 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
-const config = {
-  darkMode: ['class'],
-  content: [
-    './src/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
-    './index.html',
-  ],
-  prefix: '',
-  theme: {
-    fontSize: {
-      xs: ['0.75rem', { lineHeight: '1rem' }],
-      sm: ['0.875rem', { lineHeight: '1.5rem' }],
-      base: ['1rem', { lineHeight: '1.75rem' }],
-      lg: ['1.125rem', { lineHeight: '1.75rem' }],
-      xl: ['1.25rem', { lineHeight: '2rem' }],
-      '2xl': ['1.5rem', { lineHeight: '2.25rem' }],
-      '3xl': ['1.75rem', { lineHeight: '2.25rem' }],
-      '4xl': ['2rem', { lineHeight: '2.5rem' }],
-      '5xl': ['2.5rem', { lineHeight: '3rem' }],
-      '6xl': ['3rem', { lineHeight: '3.5rem' }],
-      '7xl': ['4rem', { lineHeight: '4.5rem' }],
-    },
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Mona Sans', ...defaultTheme.fontFamily.sans],
-        display: [
-          ['Mona Sans', ...defaultTheme.fontFamily.sans],
-          { fontVariationSettings: '"wdth" 125' },
-        ],
-      },
-      borderRadius: {
-        '4xl': '2.5rem',
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ['class'],
+    content: [
+        './index.html',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+    ],
+    prefix: '',
+    theme: {
+        fontSize: {
+            xs: ['0.75rem', { lineHeight: '1rem' }],
+            sm: ['0.875rem', { lineHeight: '1.5rem' }],
+            base: ['1rem', { lineHeight: '1.75rem' }],
+            lg: ['1.125rem', { lineHeight: '1.75rem' }],
+            xl: ['1.25rem', { lineHeight: '2rem' }],
+            '2xl': ['1.5rem', { lineHeight: '2.25rem' }],
+            '3xl': ['1.75rem', { lineHeight: '2.25rem' }],
+            '4xl': ['2rem', { lineHeight: '2.5rem' }],
+            '5xl': ['2.5rem', { lineHeight: '3rem' }],
+            '6xl': ['3rem', { lineHeight: '3.5rem' }],
+            '7xl': ['4rem', { lineHeight: '4.5rem' }],
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
         },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
+        extend: {
+            colors: {
+                accent: '#F84F39',
+            },
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                display: [
+                    ['Inter', ...defaultTheme.fontFamily.sans],
+                    { fontVariationSettings: '"wdth" 125' },
+                ],
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: '0' },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: '0' },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+            },
+        },
     },
-  },
-  plugins: [
-    require('tailwindcss-animate'),
-    ({ addVariant }) => {
-      addVariant('group-active-link', ':merge(.group).active-link &')
-    },
-  ],
+    plugins: [require('tailwindcss-animate')],
 }
-
-export default config
