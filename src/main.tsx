9be26a98b97d96b03032d614ab8da294de85a3fe
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-06 01:56:17 +0100

    Refactor main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-import { RouterProvider } from 'react-router-dom'
-import router from '@/routes'
-
-import { Refine } from '@refinedev/core'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Refine>
-      <RouterProvider router={router} />
-    </Refine>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
 
 // Remove Preload scripts loading
@@ -20,5 +14,5 @@ postMessage({ payload: 'removeLoading' }, '*')
 
 // Use contextBridge
 window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
+    console.log(message)
 })
