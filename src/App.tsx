f53e9a39101af66f57feb7089b31c5977a7235b8
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-06 01:56:14 +0100

    Refactor routing and add expense resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import { Container } from '@/components/elements/Container'
-
 import './globals.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Refine } from '@refinedev/core'
+import routerProvider from '@refinedev/react-router-v6'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-  return (
+import Layout from '@/components/modules/UI/Layout'
+
+const App = () => (
     <>
-      <Container>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="text-xl" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </Container>
-      <h1 className="text-xl">Vite + React</h1>
-      <div className="bg-yellow-400 text-4xl">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <BrowserRouter>
+            <Refine
+                resources={[
+                    {
+                        name: 'expenses',
+                        list: '/my-expenses',
+                        create: '/my-expenses/new',
+                        show: '/my-expenses/:id',
+                        edit: '/my-expenses/:id/edit',
+                        clone: '/my-expenses/:id/clone',
+                    },
+                ]}
+                routerProvider={routerProvider}
+            >
+                <Routes>
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<div>Home</div>} />
+                        <Route
+                            path="/my-expenses"
+                            element={<div>My Expenses</div>}
+                        />
+                    </Route>
+                </Routes>
+            </Refine>
+        </BrowserRouter>
     </>
-  )
-}
+)
 
 export default App
