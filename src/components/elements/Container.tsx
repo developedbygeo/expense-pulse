543603c0033c5257746475405e4c1a585a5ff625
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-06 01:56:23 +0100

    Refactor Container component

--- src/components/elements/Container.tsx
+++ src/components/elements/Container.tsx
@@ -1,27 +1,35 @@
-import { cn } from "@/lib/utils";
-import { WithClassName } from "@/types/UI";
-import clsx from "clsx";
+import clsx from 'clsx'
+
+import { cn } from '@/lib/utils'
+import { WithClassName } from '@/types/UI'
 
 type ContainerProps<T extends React.ElementType> = WithClassName & {
-  as?: T;
-  children: React.ReactNode;
-  wrapperClassName?: string;
-};
+    as?: T
+    children: React.ReactNode
+    wrapperClassName?: string
+}
 
-export const Container = <T extends React.ElementType = "div">({ 
-  as,
-  className,
-  wrapperClassName,
-  children,
+export const Container = <T extends React.ElementType = 'div'>({
+    as,
+    className,
+    wrapperClassName,
+    children,
 }: Omit<React.ComponentPropsWithoutRef<T>, keyof ContainerProps<T>> &
-  ContainerProps<T>) => {
-  const Component = as ?? "div";
+    ContainerProps<T>) => {
+    const Component = as ?? 'div'
 
-  return (
-    <Component className={clsx("mx-auto max-w-7xl px-6 lg:px-8", className)}>
-      <div className={cn("mx-auto max-w-2xl lg:max-w-none", wrapperClassName)}>
-        {children}
-      </div>
-    </Component>
-  );
-};
+    return (
+        <Component
+            className={clsx('mx-auto max-w-7xl px-6 lg:px-8', className)}
+        >
+            <div
+                className={cn(
+                    'mx-auto max-w-2xl lg:max-w-none',
+                    wrapperClassName
+                )}
+            >
+                {children}
+            </div>
+        </Component>
+    )
+}
