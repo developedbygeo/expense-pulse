756ccdfbfebfaa11642eb3c715c61abbbeb08ad3
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-10 00:18:37 +0100

    Refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
 import './globals.css'
 
-import { Refine } from '@refinedev/core'
-import routerProvider from '@refinedev/react-router-v6'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 
-import Layout from '@/components/modules/UI/Layout'
+import router from '@/routes'
 
 const App = () => (
     <>
-        <BrowserRouter>
-            <Refine
-                resources={[
-                    {
-                        name: 'expenses',
-                        list: '/my-expenses',
-                        create: '/my-expenses/new',
-                        show: '/my-expenses/:id',
-                        edit: '/my-expenses/:id/edit',
-                        clone: '/my-expenses/:id/clone',
-                    },
-                ]}
-                routerProvider={routerProvider}
-            >
-                <Routes>
-                    <Route element={<Layout />}>
-                        <Route path="/" element={<div>Home</div>} />
-                        <Route
-                            path="/my-expenses"
-                            element={<div>My Expenses</div>}
-                        />
-                    </Route>
-                </Routes>
-            </Refine>
-        </BrowserRouter>
+        {/* <Layout> */}
+        <RouterProvider router={router} />
+        {/* </Layout> */}
     </>
 )
 
