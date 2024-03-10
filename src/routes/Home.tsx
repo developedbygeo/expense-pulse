703a29890208f663e9baa5d2b9422013b7c9f5ae
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-10 16:14:18 +0100

    Add base Homepage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,21 +1,32 @@
+import { useEffect } from 'react'
+
 import { Container } from '@/components/elements/Container'
 import FadeIn from '@/components/elements/FadeIn'
 
 const Home = () => {
-    return (
-        <p className="ml-12">Hello world</p>
-        // <Container as="section">
-        //     <h1 className="sr-only">Home</h1>
+    const getData = async () => {
+        const response = await fetch('http://127.0.0.1:3000/api/createUser')
+        const data = await response.json()
+        console.log(data)
+    }
 
-        //     <FadeIn className="max-w-3xl">
-        //         <h2 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-        //             Welcome to Expense Pulse
-        //         </h2>
-        //         <p className="mt-6 text-xl text-neutral-600">
-        //             We will help you getting your money in order.
-        //         </p>
-        //     </FadeIn>
-        // </Container>
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return (
+        <Container as="section">
+            <h1 className="sr-only">Home</h1>
+
+            <FadeIn className="max-w-3xl">
+                <h2 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
+                    Welcome to Expense Pulse
+                </h2>
+                <p className="mt-6 text-xl text-neutral-600">
+                    We will help you get your finances in order.
+                </p>
+            </FadeIn>
+        </Container>
     )
 }
 
