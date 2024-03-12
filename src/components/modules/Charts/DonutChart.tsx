c1a972244c4e5865966dcd086b27f8b3d12f2e25
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-12 02:25:36 +0100

    Add DonutChart component

---
+++ src/components/modules/Charts/DonutChart.tsx
@@ -0,0 +1,30 @@
+import Chart from 'react-apexcharts'
+
+import { cn } from '@/lib/utils'
+import { WithClassName } from '@/types/UI'
+
+type DonutChartProps = WithClassName & {
+    options?: Record<string, never>
+    series: ApexNonAxisChartSeries
+    labels: string[]
+}
+
+const DonutChart = ({ className, series, labels }: DonutChartProps) => {
+    return (
+        <div className={cn('donut', className)}>
+            <Chart
+                options={{
+                    labels,
+                    tooltip: {
+                        enabled: true,
+                    },
+                }}
+                series={series}
+                type="donut"
+                width="100%"
+            />
+        </div>
+    )
+}
+
+export default DonutChart
