b2a85a4b84fc0ffb058cbcf38824521e0f12c9c6
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-12 02:26:52 +0100

    Add handleLayoutWithSidebar and colorCodeRatio functions to UI.ts

--- src/lib/UI.ts
+++ src/lib/UI.ts
@@ -1,5 +1,15 @@
+import { calculateRatio } from '@/lib/data'
+
 export const handleLayoutWithSidebar = (isOpen: boolean) => {
     isOpen
         ? document.body.classList.add('sidebar-open')
         : document.body.classList.remove('sidebar-open')
+}
+
+export const colorCodeRatio = (value: number, threshold: number) => {
+    const percentage = calculateRatio(value, threshold)
+
+    if (percentage < 70) return 'text-green-700'
+    if (percentage < 75) return 'text-amber-600'
+    return 'text-red-700'
 }
