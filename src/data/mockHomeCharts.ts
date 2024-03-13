b1b6d597086127321f8e1e12dee7a7eba53cd681
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-13 22:59:48 +0100

    Add mockHomeCharts.ts with mock data for home bar chart

---
+++ src/data/mockHomeCharts.ts
@@ -0,0 +1,16 @@
+export const mockHomeBar = {
+    options: {
+        chart: {
+            id: 'basic-bar',
+        },
+        xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+        },
+    },
+    series: [
+        {
+            name: 'series-1',
+            data: [30, 40, 45, 50, 49, 60, 70, 91],
+        },
+    ],
+}
