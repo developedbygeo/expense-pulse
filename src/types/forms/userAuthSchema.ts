302ee569ea11c5cbcef8ba180895bfa42dc0b1f5
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-14 01:21:14 +0100

    Add user authentication form schema

--- src/types/forms/userAuthSchema.ts
+++ src/types/forms/userAuthSchema.ts
@@ -1,21 +1,25 @@
 import { z, ZodType } from 'zod'
 
+import {
+    LOGIN_FIELD_NAMES,
+    REGISTER_FIELD_NAMES,
+} from '@/types/data/enums/auth'
 import { User } from '@/types/store/user'
 import { Prettify } from '@/types/UI'
 
 export type RegisterUser = Prettify<
-    Omit<User, 'UserId' | 'DateOfBirth'> & {
-        ConfirmPassword: string
+    Omit<User, 'UserId'> & {
+        [REGISTER_FIELD_NAMES.CONFIRM_PASSWORD]: string
     }
 >
 
 export type LoginUser = Pick<RegisterUser, 'Username' | 'Password'>
 
 export const LoginUserSchema: ZodType<LoginUser> = z.object({
-    Username: z.string().min(5, {
+    [LOGIN_FIELD_NAMES.USERNAME]: z.string().min(5, {
         message: 'Username must be at least 5 characters long.',
     }),
-    Password: z
+    [LOGIN_FIELD_NAMES.PASSWORD]: z
         .string()
         .min(8, { message: 'Password must be at least 8 characters long.' })
         .regex(/[0-9]/, {
@@ -25,10 +29,10 @@ export const LoginUserSchema: ZodType<LoginUser> = z.object({
 
 export const RegisterUserSchema: ZodType<RegisterUser> = z
     .object({
-        Username: z.string().min(5, {
+        [REGISTER_FIELD_NAMES.USERNAME]: z.string().min(5, {
             message: 'Username must be at least 5 characters long.',
         }),
-        Password: z
+        [REGISTER_FIELD_NAMES.PASSWORD]: z
             .string()
             .min(8, { message: 'Password must be at least 8 characters long.' })
             .regex(/[0-9]/, {
@@ -38,15 +42,17 @@ export const RegisterUserSchema: ZodType<RegisterUser> = z
                 message:
                     'Password must contain at least one special character.',
             }),
-        ConfirmPassword: z.string(),
-        FirstName: z.string(),
-        LastName: z.string(),
-        Allowance: z
+
+        [REGISTER_FIELD_NAMES.CONFIRM_PASSWORD]: z.string(),
+        [REGISTER_FIELD_NAMES.FIRST_NAME]: z.string(),
+        [REGISTER_FIELD_NAMES.LAST_NAME]: z.string(),
+        [REGISTER_FIELD_NAMES.ANNUAL_INCOME]: z.number().optional(),
+        [REGISTER_FIELD_NAMES.ALLOWANCE]: z
             .number()
             .min(100, { message: 'Allowance must be at least 100.' }),
-        ProfileImage: z.string().optional(),
+        [REGISTER_FIELD_NAMES.PROFILE_IMAGE]: z.string().optional(),
     })
-    .refine((data) => data.Password === data.ConfirmPassword, {
-        message: 'Passwords do not match.',
-        path: ['ConfirmPassword'], // This shows which field the error is associated with
+    .refine((values) => values.Password === values.ConfirmPassword, {
+        message: 'The password must match!',
+        path: ['ConfirmPassword'],
     })
