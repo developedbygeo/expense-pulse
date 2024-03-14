59d6dd9c889766b31cf5562c4a42bd90b9abec77
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-14 01:21:55 +0100

    Add auth enums for registration and login

---
+++ src/types/data/enums/auth.ts
@@ -0,0 +1,15 @@
+export enum REGISTER_FIELD_NAMES {
+    USERNAME = 'Username',
+    PASSWORD = 'Password',
+    CONFIRM_PASSWORD = 'ConfirmPassword',
+    FIRST_NAME = 'FirstName',
+    LAST_NAME = 'LastName',
+    ANNUAL_INCOME = 'AnnualIncome',
+    ALLOWANCE = 'Allowance',
+    PROFILE_IMAGE = 'ProfileImage',
+}
+
+export enum LOGIN_FIELD_NAMES {
+    USERNAME = 'Username',
+    PASSWORD = 'Password',
+}
