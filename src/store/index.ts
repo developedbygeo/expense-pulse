010d01a45c6a527e32f09d02612e0ea30d1c8858
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-14 23:57:22 +0100

    Add userApi and appSettingsSlice to store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { get } from 'http'
 
+import { userApi } from '@/store/api/user'
+import { appSettingsSlice } from '@/store/slices/appSettings'
 import { userSlice } from '@/store/slices/userSlice'
 
 const store = configureStore({
     reducer: {
         [userSlice.name]: userSlice.reducer,
+        [appSettingsSlice.name]: appSettingsSlice.reducer,
+
+        // api reducers
+        [userApi.reducerPath]: userApi.reducer,
     },
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(userApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
