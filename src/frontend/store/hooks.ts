9ecf307d49899670e6408615fae36a91000f13de
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-18 00:17:29 +0100

    Update import path in hooks.ts

--- src/frontend/store/hooks.ts
+++ src/frontend/store/hooks.ts
@@ -1,6 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
-import { AppDispatch, RootState } from '@/store'
+import { AppDispatch, RootState } from '@/store/index'
 
 export const useAppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
