3169a73af656563f4e06fb91faeefa0ae19d14ce
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-18 00:16:58 +0100

    Add core API functionality to preload script

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,32 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from 'electron'
+import { CORE_API_ACTIONS } from '@/api/enums/coreApi'
+
+contextBridge.exposeInMainWorld('core', {
+    onReceivePort: (callback: (portString: string) => void) =>
+        ipcRenderer.on(CORE_API_ACTIONS.SERVER_PORT, (_, port) => {
+            console.log('RECEIVED PORT')
+            callback(port)
+        }),
+})
+
+// `exposeInMainWorld` can't detect attributes and methods of `prototype`, manually patching it.
+function withPrototype(obj: Record<string, any>) {
+    const protos = Object.getPrototypeOf(obj)
+
+    for (const [key, value] of Object.entries(protos)) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) continue
+
+        if (typeof value === 'function') {
+            // Some native APIs, like `NodeJS.EventEmitter['on']`, don't work in the Renderer process. Wrapping them into a function.
+            obj[key] = function (...args: any) {
+                return value.call(obj, ...args)
+            }
+        } else {
+            obj[key] = value
+        }
+    }
+    return obj
+}
