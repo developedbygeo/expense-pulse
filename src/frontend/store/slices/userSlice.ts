6a454f5d06f3fae7ee3e9782a94b30ed3ab47656
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-18 02:10:15 +0100

    Add userApi to userSlice

--- src/frontend/store/slices/userSlice.ts
+++ src/frontend/store/slices/userSlice.ts
@@ -1,7 +1,8 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, isFulfilled, PayloadAction } from '@reduxjs/toolkit'
 
 import { InitialUserSliceState } from '@/types/store'
 import { User } from '@/types/store/user'
+import { userApi } from '@/frontend/store/api/user'
 
 const initialState: InitialUserSliceState = {
     currentUser: null,
@@ -18,6 +19,14 @@ export const userSlice = createSlice({
         clearUser: (state) => {
             state.currentUser = null
         },
+    },
+    extraReducers: (builder) => {
+        builder.addMatcher(
+            userApi.endpoints.getUsers.matchFulfilled,
+            (state, action) => {
+                state.availableUsers = action.payload.data
+            }
+        )
     },
 })
 
