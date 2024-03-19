610976f28b9f21ff27a3e7f72530f300928031ce
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-19 14:16:49 +0100

    Add user avatar and loading indicator to UserAccountSelect component

--- src/frontend/components/modules/User/UserAccountSelect.tsx
+++ src/frontend/components/modules/User/UserAccountSelect.tsx
@@ -1,12 +1,27 @@
 import { Button } from '@/components/elements/Button'
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from '@/frontend/components/elements/Avatar'
+import BarLoader from '@/frontend/components/elements/BarLoader'
+import { extractNameInitials } from '@/frontend/lib/data'
+import { useGetUsersQuery } from '@/frontend/store/api/user'
+import { setUser } from '@/frontend/store/slices/userSlice'
+import { User } from '@/frontend/types/store/user'
 import { cn } from '@/lib/utils'
-import { useAppSelector } from '@/store/hooks'
+import { useAppDispatch } from '@/store/hooks'
 import { WithClassName } from '@/types/UI'
 
 const UserAccountSelect = ({ className }: WithClassName) => {
-    const userAccounts = useAppSelector((state) => state.user.availableUsers)
+    const dispatch = useAppDispatch()
 
-    if (userAccounts.length === 0) {
+    const { data: users, error, isLoading } = useGetUsersQuery()
+
+    if (isLoading)
+        return <BarLoader className="py-8" content="Loading users..." />
+
+    if (users?.data?.length === 0) {
         return (
             <p
                 className={cn(
@@ -19,11 +34,28 @@ const UserAccountSelect = ({ className }: WithClassName) => {
         )
     }
 
+    const handleUserLogin = (user: User) => {
+        dispatch(setUser(user))
+    }
+
     return (
         <ul className={className}>
-            {userAccounts.map((user) => (
-                <li key={user.UserId}>
-                    <Button variant="ghost">
+            {users.data.map((user) => (
+                <li key={`${user.FirstName}-${user.LastName}-${user.UserId}`}>
+                    <Button
+                        onClick={() => handleUserLogin(user)}
+                        className="flex gap-4 w-full h-16 justify-start"
+                        variant="ghost"
+                    >
+                        <Avatar>
+                            <AvatarImage src={user.ProfileImage} />
+                            <AvatarFallback>
+                                {extractNameInitials(
+                                    user.FirstName,
+                                    user.LastName
+                                )}
+                            </AvatarFallback>
+                        </Avatar>
                         <span>
                             {user.FirstName} {user.LastName}
                         </span>
