6016480a8420ddba3e25c75a52e609e433ca953a
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-20 21:19:44 +0100

    Refactor user route file

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,32 +1,51 @@
-import express from 'express';
+import express from 'express'
 
-import { USER_ENDPOINTS } from '@/api/enums/endpoints';
-import { STATUS } from '@/api/enums/status';
-import { validateUserRegistration } from '@/api/lib/validators';
-import { UserRegistrationBody } from '@/api/types/user';
-import { insertUser } from '@/db/queries/user';
+import { USER_ENDPOINTS } from '@/api/enums/endpoints'
+import { STATUS } from '@/api/enums/status'
+import { validateUserRegistration } from '@/api/lib/validators'
+import { UserRegistrationBody } from '@/api/types/user'
+import { getAllUsers, insertUser } from '@/db/queries/user'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post(`/${USER_ENDPOINTS.REGISTER}`, validateUserRegistration, async (req, res) => {
-  const userData: UserRegistrationBody = req.body;
+router.post(
+    `/${USER_ENDPOINTS.REGISTER}`,
+    validateUserRegistration,
+    async (req, res) => {
+        const userData: UserRegistrationBody = req.body
 
-  try {
-    await insertUser(userData);
-    res.json({
-      message: 'User registered successfully',
-      status: STATUS.OK,
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: 'Error registering user',
-      status: STATUS.ERROR,
-    });
-  }
-});
+        try {
+            const data = await insertUser(userData)
+            res.json({
+                message: 'User registered successfully',
+                status: STATUS.OK,
+                data,
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: 'Error registering user',
+                status: STATUS.ERROR,
+                data: null,
+            })
+        }
+    }
+)
 
 router.post(`/${USER_ENDPOINTS.LOGIN}`, (req, res) => {
-  res.json({ message: 'Hello from LOGIN_USER', status: 200 });
-});
+    res.json({ message: 'Hello from LOGIN_USER', status: 200 })
+})
 
-export default router;
+router.get(`/${USER_ENDPOINTS.GET_USERS}`, async (req, res) => {
+    try {
+        const users = await getAllUsers()
+        res.json({
+            message: 'Users retrieved successfully',
+            status: STATUS.OK,
+            data: users,
+        })
+    } catch (err) {
+        res.status(500).json({ message: err, status: STATUS.ERROR })
+    }
+})
+
+export default router
