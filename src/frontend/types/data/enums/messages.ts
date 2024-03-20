e836b9803e726a5ff5ade8e379a0843e5dcace21
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-20 21:23:00 +0100

    Add enum for messages

---
+++ src/frontend/types/data/enums/messages.ts
@@ -0,0 +1,4 @@
+export enum MESSAGES {
+    REGISTER_SUCCESS = 'You have successfully registered. Please login to continue.',
+    REGISTER_ERROR = 'An error occurred while registering. Please try again.',
+}
