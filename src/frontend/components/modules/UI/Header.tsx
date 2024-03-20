720a1e95a06ee9d111c124d8f4e56b6171d3511c
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-20 21:21:34 +0100

    Refactor Header component to use useRef for dropdown container reference

--- src/frontend/components/modules/UI/Header.tsx
+++ src/frontend/components/modules/UI/Header.tsx
@@ -2,14 +2,21 @@ import { Avatar, AvatarFallback } from '@/components/elements/Avatar'
 import UserActions from '@/components/modules/User/UserActions'
 import { cn } from '@/lib/utils'
 import { WithClassName } from '@/types/UI'
+import { useRef } from 'react'
 
 const Header = ({ className }: WithClassName) => {
+    const headerRef = useRef<HTMLDivElement>(null)
+
     return (
         <header
-            className={cn('py-4 bg-neutral-100 dark:bg-neutral-900', className)}
+            ref={headerRef}
+            className={cn(
+                'relative py-4 bg-neutral-100 overflow-hidden',
+                className
+            )}
         >
             <div className="flex justify-end container">
-                <UserActions />
+                <UserActions dropdownContainerRef={headerRef} />
             </div>
         </header>
     )
