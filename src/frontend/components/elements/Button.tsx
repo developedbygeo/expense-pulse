621152b5aaf3f8be4098f6aae0bbad7c76c8b0f3
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-20 21:20:28 +0100

    Update Button component styles

--- src/frontend/components/elements/Button.tsx
+++ src/frontend/components/elements/Button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-[colors_transform] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 active:scale-95',
+    'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-[colors_transform] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-300 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 active:scale-95 disabled:cursor-not-allowed',
     {
         variants: {
             variant: {
