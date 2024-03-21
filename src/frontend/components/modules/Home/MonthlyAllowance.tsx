eb68bae8b241dc4829f3f02c2d317ee3eb56f228
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-21 23:23:01 +0100

    Add AllowanceDisplay component to MonthlyAllowance

--- src/frontend/components/modules/Home/MonthlyAllowance.tsx
+++ src/frontend/components/modules/Home/MonthlyAllowance.tsx
@@ -11,8 +11,30 @@ import {
 } from '@/elements/Card'
 import { cn } from '@/lib/utils'
 import { WithClassName } from '@/types/UI'
+import { useAppSelector } from '@/frontend/store/hooks'
+import withPrimitiveValueDataCheck from '@/frontend/components/modules/UI/HOC/withPrimitiveValueDataCheck'
+import PrimitiveDisplay from '@/frontend/components/elements/PrimitiveDisplay'
+import Block from '@/frontend/components/elements/Skeletons/Block'
+
+const AllowanceDisplay = withPrimitiveValueDataCheck({
+    Component: ({ className, data }) => (
+        <PrimitiveDisplay className={className} as="span">
+            <span>{data as unknown as JSX.Element}</span>
+        </PrimitiveDisplay>
+    ),
+    Skeleton: Block,
+    skeletonClassName: 'h-16 w-full',
+})
 
 const MonthlyAllowance = ({ className }: WithClassName) => {
+    const allowance = useAppSelector(
+        (state) => state.user.currentUser?.Allowance
+    )
+
+    const allowanceToRender = allowance
+        ? `${getSymbolFromCurrency('USD')} ${allowance}`
+        : undefined
+
     return (
         <Card className={cn('w-[380px]', className)}>
             <CardHeader>
@@ -25,7 +47,7 @@ const MonthlyAllowance = ({ className }: WithClassName) => {
             <CardContent className="grid gap-4">
                 <div>
                     <h3 className="text-4xl font-medium">
-                        {getSymbolFromCurrency('USD')}500
+                        <AllowanceDisplay data={allowanceToRender} />
                     </h3>
                 </div>
             </CardContent>
