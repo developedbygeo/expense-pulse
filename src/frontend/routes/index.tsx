60227bbc8f7aa62f376982642d06525a6219c720
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-21 23:22:28 +0100

    Refactor routes and add MyExpenses page

--- src/frontend/routes/index.tsx
+++ src/frontend/routes/index.tsx
@@ -1,14 +1,19 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom';
 
-import RootLayout from '@/components/modules/UI/RootLayout'
-import Home from '@/routes/Home'
+import RootLayout from '@/components/modules/UI/RootLayout';
+import Home from '@/routes/Home';
+import MyExpenses from '@/frontend/routes/MyExpenses';
+import { NAVIGATION_LINKS } from '@/frontend/data/links';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <RootLayout />,
-        children: [{ path: '/', element: <Home /> }],
+        children: [
+            { path: NAVIGATION_LINKS.HOME, element: <Home /> },
+            { path: NAVIGATION_LINKS.MY_EXPENSES, element: <MyExpenses /> },
+        ],
     },
-])
+]);
 
-export default router
+export default router;
