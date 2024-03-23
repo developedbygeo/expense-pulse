cc736844a9b6229060903b605a760f105b42b5d1
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-23 23:54:48 +0100

    Add expense enums

---
+++ src/frontend/types/data/enums/expenses.ts
@@ -0,0 +1,48 @@
+export enum EXPENSE_ACTIVITY_STATUS {
+    ACTIVE = 1,
+    INACTIVE = 0,
+}
+
+export enum EXPENSE_CATEGORY {
+    FOOD = 1,
+    TRANSPORT = 2,
+    ENTERTAINMENT = 3,
+    UTILITIES = 4,
+    GROCERIES = 5,
+    CLOTHING = 6,
+    HEALTH = 7,
+    TRAVEL = 8,
+    GIFTS_AND_DONATIONS = 9,
+    EDUCATION = 10,
+    OTHER = 11,
+}
+
+export enum EXPENSE_FREQUENCY {
+    ONE_TIME = 1,
+    DAILY = 2,
+    WEEKLY = 3,
+    MONTHLY = 4,
+    YEARLY = 5,
+}
+
+export enum EXPENSE_PAYMENT_METHOD {
+    CASH = 1,
+    CREDIT_CARD = 2,
+    DEBIT_CARD = 3,
+    BANK_TRANSFER = 4,
+    PAYPAL = 5,
+    CRYPTOCURRENCY = 6,
+}
+
+export enum EXPENSE_PAYMENT_CURRENCY {
+    EUR = 1,
+    USD = 2,
+    GBP = 3,
+    JPY = 4,
+    CNY = 5,
+    INR = 6,
+    AUD = 7,
+    CAD = 8,
+    CHF = 9,
+    SEK = 10,
+}
