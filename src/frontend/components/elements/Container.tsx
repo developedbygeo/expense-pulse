eecc57767dd255dd118c19eddba9b265c43fb65a
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-23 23:53:49 +0100

    Update Container component styling

--- src/frontend/components/elements/Container.tsx
+++ src/frontend/components/elements/Container.tsx
@@ -1,13 +1,13 @@
-import clsx from 'clsx'
+import clsx from 'clsx';
 
-import { cn } from '@/lib/utils'
-import { WithClassName } from '@/types/UI'
+import { cn } from '@/lib/utils';
+import { WithClassName } from '@/types/UI';
 
 type ContainerProps<T extends React.ElementType> = WithClassName & {
-    as?: T
-    children: React.ReactNode
-    wrapperClassName?: string
-}
+    as?: T;
+    children: React.ReactNode;
+    wrapperClassName?: string;
+};
 
 export const Container = <T extends React.ElementType = 'div'>({
     as,
@@ -16,11 +16,11 @@ export const Container = <T extends React.ElementType = 'div'>({
     children,
 }: Omit<React.ComponentPropsWithoutRef<T>, keyof ContainerProps<T>> &
     ContainerProps<T>) => {
-    const Component = as ?? 'div'
+    const Component = as ?? 'div';
 
     return (
         <Component
-            className={clsx('mx-auto max-w-7xl px-6 lg:px-8', className)}
+            className={clsx('mx-auto max-w-10xl px-6 lg:px-8', className)}
         >
             <div
                 className={cn(
@@ -31,5 +31,5 @@ export const Container = <T extends React.ElementType = 'div'>({
                 {children}
             </div>
         </Component>
-    )
-}
+    );
+};
