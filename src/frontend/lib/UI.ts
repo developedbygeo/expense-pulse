31f5f927f5da04ffa12900b33eba1b056e324797
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-23 23:54:37 +0100

    Add UI helper functions and types

--- src/frontend/lib/UI.ts
+++ src/frontend/lib/UI.ts
@@ -1,18 +1,28 @@
-import { calculateRatio } from '@/lib/data'
+import { SelectItemType } from '@/frontend/types/UI';
+import { calculateRatio } from '@/lib/data';
 
 export const handleLayoutWithSidebar = (isOpen: boolean) => {
     isOpen
         ? document.body.classList.add('sidebar-open')
-        : document.body.classList.remove('sidebar-open')
-}
+        : document.body.classList.remove('sidebar-open');
+};
 
 export const colorCodeRatio = (value: number, threshold: number) => {
-    const percentage = calculateRatio(value, threshold)
+    const percentage = calculateRatio(value, threshold);
 
-    if (percentage < 70) return 'text-green-700'
-    if (percentage < 75) return 'text-amber-600'
-    return 'text-red-700'
-}
+    if (percentage < 70) return 'text-green-700';
+    if (percentage < 75) return 'text-amber-600';
+    return 'text-red-700';
+};
 
 export const generateCtaAsyncText = (text: string, isLoading: boolean) =>
-    isLoading ? 'Loading...' : text
+    isLoading ? 'Loading...' : text;
+
+export const generateArrayFromEnum = <T>(enumObject: T): SelectItemType[] => {
+    return Object.entries(enumObject)
+        .filter(([key]) => isNaN(Number(key))) // Filter out the numeric keys
+        .map(([key, value]) => ({
+            value: value.toString(),
+            label: key,
+        }));
+};
