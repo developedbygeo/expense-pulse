b5b0714fb9c25afe28c22f21808d2e9b7b1e0532
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-31 14:34:46 +0200

    Formatting

--- src/frontend/store/slices/userSlice.ts
+++ src/frontend/store/slices/userSlice.ts
@@ -1,35 +1,35 @@
-import { createSlice, isFulfilled, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { InitialUserSliceState } from '@/types/store'
-import { User } from '@/types/store/user'
-import { userApi } from '@/frontend/store/api/user'
+import { InitialUserSliceState } from '@/types/store';
+import { User } from '@/types/store/user';
+import { userApi } from '@/frontend/store/api/user';
 
 const initialState: InitialUserSliceState = {
     currentUser: null,
     availableUsers: [],
-}
+};
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
         setUser: (state, action: PayloadAction<User>) => {
-            state.currentUser = action.payload
+            state.currentUser = action.payload;
         },
         clearUser: (state) => {
-            state.currentUser = null
+            state.currentUser = null;
         },
     },
     extraReducers: (builder) => {
         builder.addMatcher(
             userApi.endpoints.getUsers.matchFulfilled,
             (state, action) => {
-                state.availableUsers = action.payload.data
+                state.availableUsers = action.payload.data;
             }
-        )
+        );
     },
-})
+});
 
-export const { setUser, clearUser } = userSlice.actions
+export const { setUser, clearUser } = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
