390701e57a7c825e94cc8de9558ddacb2c2d8dbf
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-31 14:33:39 +0200

    Refactor RootLayout component

--- src/frontend/components/modules/UI/RootLayout.tsx
+++ src/frontend/components/modules/UI/RootLayout.tsx
@@ -1,18 +1,18 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 
-import Header from '@/components/modules/UI/Header'
-import Sidebar from '@/components/modules/UI/Sidebar'
-import CurrentUserLogin from '@/components/modules/User/CurrentUserLogin'
+import Header from '@/components/modules/UI/Header';
+import Sidebar from '@/components/modules/UI/Sidebar';
+import CurrentUserLogin from '@/components/modules/User/CurrentUserLogin';
 
 const RootLayout = () => (
     <div className="relative bg-neutral-50 dark:bg-neutral-800 min-h-screen">
         <Header />
         <Sidebar className="fixed top-0 left-0 h-screen" />
         <CurrentUserLogin />
-        <main className="ml-12 mt-6 transition-transform duration-500 ease-in-out container">
+        <main className="ml-12 mt-6 transition-transform duration-500 ease-in-out">
             <Outlet />
         </main>
     </div>
-)
+);
 
-export default RootLayout
+export default RootLayout;
