ab7d670d1aa8b1948286db552a894779b56b9645
developedbygeo <georgiosanagnostou@hotmail.com>
2024-03-31 14:34:19 +0200

    Refactor Home component and update greeting message

--- src/frontend/routes/Home.tsx
+++ src/frontend/routes/Home.tsx
@@ -1,29 +1,27 @@
 import { BentoGrid, BentoGridItem } from '@/components/elements/BentoGrid';
 import { Container } from '@/components/elements/Container';
 import FadeIn from '@/components/elements/FadeIn';
-import { items } from '@/data/mockHomeItems';
 import CurrentUserGreeting from '@/frontend/components/modules/User/CurrentUserGreeting';
 import { cn } from '@/lib/utils';
 import { Fragment } from 'react/jsx-runtime';
 
+import homeModules from '@/frontend/components/modules/Home/HomeModules';
+import { useAppSelector } from '@/frontend/store/hooks';
+
 const Home = () => {
+    // const expenseData = useAppSelector(state => state.)
+
     return (
         <Container as="section">
             <h1 className="sr-only">Home</h1>
 
             <FadeIn className="">
-                <h2 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-                    <CurrentUserGreeting
-                        text="Welcome to Expense Pulse"
-                        fallbackText="Welcome to Expense Pulse"
-                        as="span"
-                    />
-                </h2>
-                <p className="mt-6 text-xl text-neutral-600">
-                    We will help you get your finances in order.
-                </p>
+                <CurrentUserGreeting
+                    text="Welcome to Expense Pulse"
+                    description="We will help you get your finances in order."
+                />
                 <BentoGrid className="apply-mb mt-12">
-                    {items.map((item, i) => (
+                    {homeModules.map((item, i) => (
                         <BentoGridItem
                             key={i}
                             title={item.title}
